c48dfae4dc8fa4fbe67162560452324e07e1eff5
David Behar <davbelom@gmail.com>
2019-12-19 23:37:34 -0600

    Infinite program loop

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def create_client(client_name):
 def update_client(client_name,updated_client_name):
     global clients
 
-    if client_name in clients:
+    if client_name.upper() in clients.upper():
         clients=clients.replace(client_name+',',updated_client_name+',')
         list_clients()
     else:
@@ -47,12 +47,11 @@ def search_client(client_name):
     clients_list = clients.split(',')
 
     for client in clients_list:
-        if client == client_name:
+        if client.upper() == client_name.upper():
             return True
 
     return False
-
-    # return client_name in clients
+    # return client_name in clients # This line does the same
 
 
 def _add_trailing_comma():
@@ -62,7 +61,10 @@ def _add_trailing_comma():
 
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name=None
+    while not client_name:
+        client_name = input('What is the client name? ')
+    return client_name
 
 
 def _client_not_found(client_name):
@@ -74,6 +76,7 @@ def _client_not_found(client_name):
 
 
 def print_welcome():
+    print('\n'*3)
     print('Welcome to my store')
     print('*'*50)
     print('What would you like to do today?')
@@ -83,39 +86,47 @@ def print_welcome():
     print('[D]elete client')
     print('[S]earch client')
     print('[Q]uit')
+    print('')
 
 
 if __name__ == "__main__":
-    print_welcome()
 
-    command = input()
-    command = command.upper()
+    while True:
 
-    if command=='C':
-        client_name=_get_client_name()
-        create_client(client_name)
-        list_clients()
-    elif command=='R':
-        list_clients()
-    elif command=='L':
-        list_clients()
-    elif command=='U':
-        client_name=_get_client_name()
-        updated_client_name=input('What is the updated client name? ')
-        update_client(client_name,updated_client_name)
-    elif command=='D':
-        client_name=_get_client_name()
-        delete_client(client_name)
-    elif command=='S':
-        client_name=_get_client_name()
-        found=search_client(client_name)
+        command=False
+        while not command:
 
-        if found:
-            print('Client '+client_name+' is in the client list')
+            print_welcome()
+            
+            command = input()
+            command = command.upper()
+
+        if command=='C':
+            client_name=_get_client_name()
+            create_client(client_name)
+            list_clients()
+        elif command=='R':
+            list_clients()
+        elif command=='L':
+            list_clients()
+        elif command=='U':
+            client_name=_get_client_name()
+            updated_client_name=input('What is the updated client name? ')
+            update_client(client_name,updated_client_name)
+        elif command=='D':
+            client_name=_get_client_name()
+            delete_client(client_name)
+        elif command=='S':
+            client_name=_get_client_name()
+            found=search_client(client_name)
+
+            if found:
+                print('Client '+client_name+' is in the client list')
+            else:
+                print('Client {} is not in the client\s list'.format(client_name))
+
+        elif command=='Q':
+            print('Thank you for your time, please come back later')
+            break
         else:
-            print('Client {} is not in the client\s list'.format(client_name))
-
-    elif command=='Q':
-        print('Thank you for your time, please come back later')
-    else:
-        print('Command is invalid')
+            print('Command is invalid')
